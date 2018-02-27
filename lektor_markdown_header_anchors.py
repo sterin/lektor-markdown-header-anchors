52577ffef006afe1560f22a7cb3a7898835f723d
Baruch Sterin <baruchs@gmail.com>
2018-02-26 19:37:18 -0800

    Ensurs that anchos are unique: appends ~2, ~3, to anchors that would otherwise repeat.

--- lektor_markdown_header_anchors.py
+++ lektor_markdown_header_anchors.py
@@ -2,11 +2,20 @@ from lektor.pluginsystem import Plugin
 import uuid
 from lektor.utils import slugify
 from markupsafe import Markup
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 
 TocEntry = namedtuple('TocEntry', ['anchor', 'title', 'children'])
 
+
+def unique_anchor(slug_counter, raw):
+    """ Return a unique anchor """
+    slug = slugify(raw)
+    slug_counter[slug] += 1
+    if slug_counter[slug] > 1:
+        return '{}~{}'.format(slug, slug_counter[slug])
+    return slug
+  
 
 class MarkdownHeaderAnchorsPlugin(Plugin):
     name = 'Markdown Header Anchors'
@@ -18,13 +27,14 @@ class MarkdownHeaderAnchorsPlugin(Plugin):
                 if self.get_config().get('anchor-type') == "random":
                     anchor = uuid.uuid4().hex[:6]
                 else:
-                    anchor = slugify(raw)
+                    anchor = unique_anchor(renderer.meta['anchor_slug_counter'], raw)
                 renderer.meta['toc'].append((level, anchor, Markup(text)))
                 return '<h%d id="%s">%s</h%d>' % (level, anchor, text, level)
         config.renderer_mixins.append(HeaderAnchorMixin)
 
     def on_markdown_meta_init(self, meta, **extra):
         meta['toc'] = []
+        meta['anchor_slug_counter'] = Counter()
 
     def on_markdown_meta_postprocess(self, meta, **extra):
         prev_level = None
